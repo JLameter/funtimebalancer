d6aef2c53511f801301e0fb857013f9b0869fc17
Jonathan E Lameter <jonathan@lameter.com>
2025-05-10 10:16:39 -0500

    TEMP - Moving to Debian

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 """Launch Module for the app"""
 
 import time
+import pygame
 
 
-def start_timer(interval, debug_mode=False):
+def start_timer(interval, volume, debug_mode=False):
     """Run the timer"""
     print("starting timer...")
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound("coin.mp3")
+    sound.set_volume(volume)
     coins = 0
     try:
         while True:
             time.sleep(interval)
             coins += 1
             print("Gained 1 coin! That's", coins, "coins total!")
+            sound.play()
             if debug_mode:
-                print("Interval:", interval)
+                print("Interval:", interval, "seconds")
     except KeyboardInterrupt:
         print("Timer stopped with", coins, "coins")
 
 
-start_timer(2, True)
+start_timer(2, 0.1, True)
